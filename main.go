ce0152358dcf8ccfc396fab73dd43ade7447f0c5
zack <zb01406@gmail.com>
2024-01-07 11:16:48 -0500

    remove comments

--- main.go
+++ main.go
@@ -8,14 +8,13 @@ import (
 )
 
 func main() {
-	//open csv
 	file, err := os.Open("event_schedule.csv")
 	if err != nil {
 		log.Fatal("error opening  csv file", err)
 	}
-	defer file.Close() //make sure to close file, no matter how the func exits
 
-	// create new csv reader and read entire file
+	defer file.Close()
+
 	csvReader := csv.NewReader(file)
 	data, err := csvReader.ReadAll()
 
@@ -23,7 +22,6 @@ func main() {
 		log.Fatal("error reading csv file", err)
 	}
 
-	//loop over csv
 	for _, value := range data {
 		fmt.Println(value)
 	}
