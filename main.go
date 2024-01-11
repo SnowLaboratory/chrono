ac7424d3d2770609112e281cc136152d22a9ae70
zack <zb01406@gmail.com>
2024-01-11 18:49:09 -0500

    switch to db

--- main.go
+++ main.go
@@ -1,21 +1,97 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
-	"snowlabs/chrono/lib"
+	"net/http"
+	"os"
+	"text/template"
 
-	"github.com/gin-gonic/gin"
+	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
+var db *sql.DB
+
+type Event struct {
+	Event    string
+	Start    string
+	End      string
+	Platform string
+	ID       int
+}
+
 func main() {
-	_, err := lib.ExportCsvToJson()
+	cfg := mysql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 "localhost:3306",
+		DBName:               "chrono",
+		AllowNativePasswords: true,
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
-	router := gin.Default()
-	router.GET("/events", func(c *gin.Context) {
-		c.Header("Content-Type", "application/json")
-		c.File("event_data.json")
-	})
-	router.Run()
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+	// Set up API routes
+	r := mux.NewRouter()
+	r.HandleFunc("/events", getAllEvents).Methods("GET")
+	r.HandleFunc("/", homeHandler).Methods("GET")
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func getAllEvents(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT * FROM events")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var events []Event
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.Event, &event.Start, &event.End, &event.Platform, &event.ID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		events = append(events, event)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Convert the events slice to JSON
+	responseJSON, err := json.Marshal(events)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON response
+	w.Write(responseJSON)
 }
